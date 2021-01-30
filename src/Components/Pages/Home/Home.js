d1ea3ad3adb3b16f853fc71e25ef7a3178e8d3d7
rflynn523 <rflynn523@yahoo.com>
2021-01-30 12:13:26 -0500

    Styled the position of the homepage images & intro

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -2,13 +2,30 @@ import React from 'react';
 import Button from '../../Button/Button'
 import '../Home/Home.css'
 
+import trees from '../../../images/BackTreesWebSize.jpg';
+import engBuilding from '../../../images/ENGBuildingWebSize.jpg';
+import cap from '../../../images/CapThrowWebSize.jpg';
+
+
+
 class Home extends React.Component
 {
     render()
     {
         return(
-            <div className="grid-pageList">
-                this is the home page
+            <div className="homeGrid">
+                <p>
+                    A young engineer with a growing passion for software development, embedded systems, and compute science.
+                    I recently graduated from the University of Central Florida and started working working as a Computer 
+                    Programming Intern at Cuhaci & Peterson.
+                </p>
+
+                {/* <img src = {trees} /> */}
+
+                <img src = {engBuilding} />
+                {/* <img src = {cap} /> */}
+
+
             </div>
         )
     }
