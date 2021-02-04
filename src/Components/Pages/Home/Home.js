aa67a3b23c3d3fe820b02e5ba9c9aa3955f4c70d
rflynn523 <rflynn523@yahoo.com>
2021-02-03 21:23:32 -0500

    Added home page description

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -15,9 +15,15 @@ class Home extends React.Component
         return(
             <div className="homeGrid">
                 <p>
-                    A young engineer with a growing passion for software development, embedded systems, and compute science.
-                    I recently graduated from the University of Central Florida and started working working as a Computer 
-                    Programming Intern at Cuhaci & Peterson.
+                    <h2>About Me</h2>
+                    A young engineer with a growing passion for creating software and learning new about new 
+                    technology. I recently graduated from UCF in December of 2020 and start working as a 
+                    Computer Program Intern at Cuhaci & Peterson where I am currently employed. This web app
+                    serves as an extension of my Resume and also gave me the opportunity to learn ReactJS. It 
+                    features more information on my education along with interactive Projects and Skills pages.
+
+                    <h3>Contact Info</h3>
+                    <div>This is my contact info</div>
                 </p>
 
                 {/* <img src = {trees} /> */}
