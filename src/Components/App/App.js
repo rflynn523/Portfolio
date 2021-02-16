60d33b986e2ceb319c26fb9c70c0601fe053b6b4
rflynn523 <rflynn523@yahoo.com>
2021-02-15 21:56:39 -0500

    Set all the same font

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -70,15 +70,13 @@ class App extends React.Component
   {
     return (
       <div className="App">
-        {/* Include all of the hrefs here could prob move them to index? 
-            All Not needed???   */}
-        <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC&family=Sura&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/>
+        {/* <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC&family=Sura&display=swap" rel="stylesheet"/> */}
+         {/* <link href="https://fonts.googleapis.com/css2?family=Carrois+Gothic+SC&display=swap" rel="stylesheet"/> */}
+         {/* <link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet"/> */}
+         {/* <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/> */}
 
-        <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet"/>
+        {/* <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet"/> */}
+        {/* <link href="https://fonts.googleapis.com/css2?family=Merienda&display=swap" rel="stylesheet"/> */}
         <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet"/>
 
 
