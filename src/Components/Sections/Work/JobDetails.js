1c8f56641a711280134b89632a8625840835ae29
rflynn523 <47935351+rflynn523@users.noreply.github.com>
2023-07-20 22:08:04 -0400

    Work layout (#3)
    * initial layout
    
    * Implemented Accordions for the work section
    
    * Created JobDetails component

---
+++ src/Components/Sections/Work/JobDetails.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactGA from 'react-ga'
+
+import { Chip, Grid, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+class JobDetails extends React.Component
+{
+    
+    render()
+    {
+        return (
+            
+            <Grid container spacing={2} alignItems="center">
+
+                {/* Bullet Points */}
+                <Grid item xs={12} md={12}>
+                    <List sx={{ listStyleType: 'disc' }}>
+                    {this.props.job_info.bullet_points.map((bullet) => (
+
+                        <ListItem>
+                            <ListItemIcon >
+                                <FiberManualRecordIcon color="black" />
+                            </ListItemIcon>
+
+                            <ListItemText primary={bullet} />
+                        </ListItem>
+                    ))}
+                    </List> 
+                </Grid>
+
+                {/* Chips with the tech used */}
+                <Grid item xs={12}>
+                    <div style={{ display: 'flex', gap: 8, flexWrap: 'wrap' }}>
+                        {this.props.job_info.technology.map((tech) => 
+                            <Chip label={tech} />
+                        )}
+                    </div>
+                </Grid>
+            </Grid>
+            );
+        };   
+    }
+
+export default JobDetails;
--- src/Components/Sections/Work/Work.js
+++ src/Components/Sections/Work/Work.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactGA from 'react-ga'
 
 import './Work.css';
-import ListCard from '../../Card/ListCard';
+import JobSummary from './JobSummary';
+import JobDetails from './JobDetails';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Box, Grid, Container } from '@mui/material';
+
 
 // Can update all of the Work Experience Information in this Component and it will be passed through props
 
@@ -13,34 +21,36 @@ class Work extends React.Component
     solute = 
     {
         title : "Software Engineer",
+        company : "SOLUTE",
         date : "Aug 2021 - Present",
-        description : 
-                    "Backend work in Python, included various feature work, creating unit tests, adding data validation with pydantic." +
-                    "Also worked in a team to perform merge requests, debug errors before releases, and come up with new ideas for features/improvements." +
-                    "When the team needed it, spent time doing DevOps work to get Redmine running in a RKE2 environment." +
-                    "Developed a microservice in Golang that parses messages using Kafka in a publish-subscribe architecture." + 
-                    "Documented legacy build and configuration systems using Gitlab Wiki. " +
-                    "Working on AGILE development schedule with 2-week sprints, managing issues with Gitlab. ",
-        technology : ["Python - Postgres - Pydantic - Swagger - GitLab"],
+        bullet_points : ["Backend work in Python, included various feature work, creating unit tests, adding data validation with pydantic.",
+                    "Also worked in a team to perform merge requests, debug errors before releases, and come up with new ideas for features/improvements.",
+                    "When the team needed it, spent time doing DevOps work to get Redmine running in a RKE2 environment.",
+                    "Developed a microservice in Golang that parses messages using Kafka in a publish-subscribe architecture.", 
+                    "Documented legacy build and configuration systems using Gitlab Wiki. ",
+                    "Working on AGILE development schedule with 2-week sprints, managing issues with Gitlab. "],
+        technology : ["Python", "Postgres", "Pydantic", "Swagger", "GitLab"],
         links : "SOLUTE A Sigma Defense Company" // Any links that I can share but probably none/ Use for the company name
     }
 
     CP = 
     {
         title : "Computational Design Programmer",
+        company : "Cuhaci & Peterson Architects",
         date : "Jan 2021 - Aug 2021",
-        description : "Developed automation scripts that create Revit models for teams within the company. " + 
-                        "Used Computational Design to incorporate intelligence to make workflows more efficient. " + 
-                        "Built team specific software such as a desktop GUI for teams to record locations of site photos.",
-        technology : ["360 Photo Mapper - Python - Dynamo"],
+        bullet_points : ["Developed automation scripts that create Revit models for teams within the company. ", 
+                        "Used Computational Design to incorporate intelligence to make workflows more efficient. ",
+                        "Built team specific software such as a desktop GUI for teams to record locations of site photos."],
+        technology : ["360 Photo Mapper", "Python", "Dynamo"],
         links : "Cuhaci & Peterson Architects" // Any links that I can share but probably none/ Use for the company name
     }
 
     shipt =
     {
         title : "Shipt Shopper",
+        company : "Shipt",
         date : "Feb 2017 - Dec 2020",
-        description : "Shopped for and delivered groceries to the customer with in the delivery window. Along the way I would comunicate with the customer about the status of their items.",
+        bullet_points : ["Shopped for and delivered groceries to the customer with in the delivery window. Along the way I would comunicate with the customer about the status of their items."],
         technology : [],
         links : "Shipt"
     }
@@ -48,8 +58,9 @@ class Work extends React.Component
     nass = 
     {
         title : "Special Projects Assistant",
+        company : "NASS",
         date : "Mar 2017 - Sep 2018",
-        description : "Performed various tasks that came up, which included employee computer setup and configuration, data entry, and budget organization.",
+        bullet_points : ["Performed various tasks that came up, which included employee computer setup and configuration, data entry, and budget organization."],
         technology : [],
         links : "North American Sub Station Services"
     }
@@ -57,8 +68,9 @@ class Work extends React.Component
     summerCamp = 
     {
         title : "Summer Camp Counselor",
+        company : "Hunter's Creek Community Association",
         date : "Summer 2017 & 2018",
-        description : "Worked as a team with the other counselors to provide fun and safe activities for the campers",
+        bullet_points : ["Worked as a team with the other counselors to provide fun and safe activities for the campers"],
         technology : [],
         links : "Hunter's Creek Community Association"
     }
@@ -75,7 +87,26 @@ class Work extends React.Component
             <div className='borderBox'>
                 <h2>Work Section</h2>
                 <div className="work">
-                    <ListCard allObjects = {this.allWork} componentType = "Work"/>
+                    <Container maxWidth="lg">
+                        {this.allWork.map((job, index) => (
+                            <Grid item xs={12} sm={6} md={4} key={index}>
+                                <Box marginBottom={2} >
+                                    <Accordion container spacing={2} alignItems="center">
+
+                                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" >
+                                            <JobSummary job_info = {job}/>
+                                        </AccordionSummary>
+
+                                        <AccordionDetails>
+                                            <JobDetails job_info = {job}/>
+                                        </AccordionDetails>
+                    
+                                    </Accordion>
+                                </Box>
+                            </Grid>
+                            ))}
+                    </Container>
+
                 </div>
             </div>
         )       
