24d905a4a38bf585bce090856283a35fa0b1d8e9
rflynn523 <rflynn523@yahoo.com>
2024-07-12 22:40:33 -0400

    tool icons

--- src/Components/Sections/AboutMe/AboutMe.js
+++ src/Components/Sections/AboutMe/AboutMe.js
@@ -18,6 +18,12 @@ import c from "../../../images/tech_logos/languages/c-logo.png";
 // Libraries / Frameworks
 
 // Tools
+import vscode from "../../../images/tech_logos/tools/vscode-logo.png";
+import xcode from "../../../images/tech_logos/tools/xcode-logo.svg";
+import android_studio from "../../../images/tech_logos/tools/android-studio-logo.png";
+import pycharm from "../../../images/tech_logos/tools/pycharm-logo.png";
+import postman from "../../../images/tech_logos/tools/postman-logo.png";
+import swagger from "../../../images/tech_logos/tools/swagger-logo.svg";
 
 // Hardware Programming?
 
@@ -52,14 +58,7 @@ class AboutMe extends React.Component {
 
   tools = {
     title: "Tools",
-    logos: [
-      // VS Code
-      // XCode
-      // Android Studio
-      // PyCharm
-      // Postman
-      // Swagger
-    ],
+    logos: [vscode, xcode, android_studio, pycharm, postman, swagger],
   };
 
   databases = {
@@ -101,56 +100,53 @@ class AboutMe extends React.Component {
         <Typography variant="h4" align="center" gutterBottom>
           About Me
         </Typography>
-          <div className="flex items-center p-32">
+        <div className="flex items-center p-32">
+          {/* Picture on the left */}
+          <div className="flex-shrink-0">
+            <img
+              src={engBuilding}
+              alt="Image"
+              className="size-128 rounded-full object-cover"
+            />
+          </div>
 
-            {/* Picture on the left */}
-            <div className="flex-shrink-0">
-              <img
-                src={engBuilding}
-                alt="Image"
-                className="size-128 rounded-full object-cover"
-                />
-              </div>
+          {/* Intro paragraph on the right */}
+          <div className="ml-12">
+            <Typography
+              className="bg-slate bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg text-app-text-color"
+              variant="body1"
+              paragraph
+            >
+              A young engineer with a growing passion for creating software and
+              learning about new technology. I recently graduated from UCF in
+              December of 2020 and I am currently working as a Software Engineer
+              at SOLUTE. My most recent personal project is an IOS app called
+              "Savings Mapper". You can check it out on the App Store.
+            </Typography>
 
-            {/* Intro paragraph on the right */}
-            <div className="ml-12">
+            <Typography
+              className="bg-slate bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg text-app-text-color"
+              variant="body1"
+              paragraph
+            >
+              This web app serves as an extension of my resume and also gave me
+              the opportunity to learn ReactJS. It features more information
+              displayed in a more interactive way. I am now using it to
+              personally track my growth as a developer through my projects,
+              skills, and work experience.
+            </Typography>
+          </div>
+        </div>
 
-              <Typography
-                className="bg-slate bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg text-app-text-color"
-                variant="body1"
-                paragraph
-                >
-                A young engineer with a growing passion for creating software
-                and learning about new technology. I recently graduated from UCF
-                in December of 2020 and I am currently working as a Software
-                Engineer at SOLUTE. My most recent personal project is an IOS
-                app called "Savings Mapper". You can check it out on the App
-                Store.
-              </Typography>
-
-              <Typography
-                className="bg-slate bg-opacity-10 backdrop-blur-lg p-8 rounded-lg shadow-lg text-app-text-color"
-                variant="body1"
-                paragraph
-                >
-                This web app serves as an extension of my resume and also gave
-                me the opportunity to learn ReactJS. It features more
-                information displayed in a more interactive way. I am now using
-                it to personally track my growth as a developer through my
-                projects, skills, and work experience.
-              </Typography>
-              </div>
+        {/* Grid for the Tech Used */}
+        <div className="grid grid-cols-3 gap-10 mx-24">
+          {this.card_info.map((card, index) => (
+            <div key={card.id}>
+              {/* Add Props to TechUsedCard to fill with the icons and probably the info needed for the grids */}
+              <TechUsedCard title={card.title} logos={card.logos} />
             </div>
-
-            {/* Grid for the Tech Used */}
-            <div className="grid grid-cols-3 gap-10 mx-24">
-              {this.card_info.map((card, index) => (
-                <div key={card.id}>
-                  {/* Add Props to TechUsedCard to fill with the icons and probably the info needed for the grids */}
-                  <TechUsedCard title={card.title} logos={card.logos} />
-                </div>
-              ))}
-            </div>
+          ))}
+        </div>
       </div>
     );
   }
