cbf6f72873677abb5a7acb135c5c3a25fb3b73a9
rflynn523 <rflynn523@yahoo.com>
2024-07-18 22:28:29 -0400

    tailwinds for TechUsedCard

--- src/Components/Card/TechUsedCard.js
+++ src/Components/Card/TechUsedCard.js
@@ -1,31 +1,7 @@
 import React from "react";
-import ReactGA from "react-ga";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import Button from "@mui/material/Button";
-import Divider from "@mui/material/Divider";
-import Box from "@mui/material/Box";
-
-import Typography from "@mui/material/Typography";
-import test from "../../images/appStoreIcon.png";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import { IconButton, makeStyles } from "@mui/material";
-import { Favorite, Share, Comment } from "@mui/icons-material";
-import { FaBeer, FaCoffee, FaApple } from "react-icons/fa";
-import "./Card.css";
+import Tooltip from "../Sections/AboutMe/ToolTip";
 
 import "./Card.css";
-
-const imageStyle = {
-  objectFit: "cover",
-  // borderRadius: '4px',
-  margin: "5px",
-  // width: '70px',
-  height: "70px",
-};
 
 class TechUsedCard extends React.Component {
   constructor(props) {
@@ -39,14 +15,15 @@ class TechUsedCard extends React.Component {
           {this.props.title}
         </div>
         <div className="flex flex-wrap gap-4 justify-evenly">
-          {this.props.logos.map((image, imageIndex) => {
+          {this.props.logos.map((logo, logoIndex) => {
             return (
-              <img
-                src={image}
-                alt={`Image ${imageIndex + 1}`}
-                key={imageIndex}
-                className='size-16 object-contain"'
-              />
+              <Tooltip key={logoIndex} text={logo.tooltip}>
+                <img
+                  src={logo.image}
+                  alt={`Image ${logoIndex + 1}`}
+                  className="size-16 object-contain"
+                />
+              </Tooltip>
             );
           })}
         </div>
