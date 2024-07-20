66fe8e899b91da06190383c19dd1d0ecb61bb3cb
rflynn523 <rflynn523@yahoo.com>
2024-07-19 22:53:19 -0400

    new nav bar layout

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,22 +1,56 @@
 import React from "react";
 import "./NavBar.css";
-import Button from "../Button/Button";
+import { FaLinkedin, FaGithub, FaFilePdf } from "react-icons/fa";
 
-// NavBar Component, contains the listed out tab Buttons
-// (App -> NavBar)
-//  - isActive, activeComponent, onClick
+const scrollToSection = (sectionId) => {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
 class NavBar extends React.Component {
   render() {
     return (
-      <div className="grid-navBar">
-        <div className="name">Ryan Flynn</div>
-
-        <Button id="Home" />
-        <Button id="About Me" />
-        <Button id="Projects" />
-        <Button id="Work" />
-        <Button id="Contact" />
-      </div>
+      <nav className="bg-slate bg-opacity-10 backdrop-blur-lg p-5 text-app-text-color">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex flex-1 space-x-4">
+            <button onClick={() => scrollToSection("Home")} className="text-app-text-color">Home</button>
+            <button onClick={() => scrollToSection("About Me")} className="text-app-text-color">About Me</button>
+            <button onClick={() => scrollToSection("Projects")} className="text-app-text-color">Projects</button>
+            <button onClick={() => scrollToSection("Work")} className="text-app-text-color">Work</button>
+          </div>
+          <div className="text-app-text-color text-2xl font-bold absolute left-1/2 transform -translate-x-1/2">
+            Ryan Flynn
+          </div>
+          <div className="flex space-x-4">
+            <a
+              href="https://www.linkedin.com/in/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://github.com/yourprofile"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="link-to-your-resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white"
+            >
+              <FaFilePdf />
+            </a>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
