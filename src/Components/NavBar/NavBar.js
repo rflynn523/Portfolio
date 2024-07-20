1aed223a27924b2bff9bdd6f4889e93671ed5098
rflynn523 <rflynn523@yahoo.com>
2024-07-19 23:09:10 -0400

    removed Contact section

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,16 @@
 import React from "react";
+import ReactGA from "react-ga";
+
 import "./NavBar.css";
 import { FaLinkedin, FaGithub, FaFilePdf } from "react-icons/fa";
+import mailIcon from "../../images/envelope.png";
+import savingsMapperPrivacyPolicy from "./Privacy Policy.pdf";
+import resumePDF from "./Ryan Flynn Resume.pdf";
+
+import linkedInIcon from "../../images/LI-In-Bug.png";
+import githubIcon from "../../images/GitHub-Logo.png";
+import resumeIcon from "../../images/resumeIcno.png";
+
 
 const scrollToSection = (sectionId) => {
   const section = document.getElementById(sectionId);
@@ -11,6 +21,9 @@ const scrollToSection = (sectionId) => {
 
 class NavBar extends React.Component {
   render() {
+    // To report page view
+    ReactGA.pageview("/Contact");
+
     return (
       <nav className="bg-slate bg-opacity-10 backdrop-blur-lg p-5 text-app-text-color">
         <div className="container mx-auto flex justify-between items-center">
@@ -25,29 +38,41 @@ class NavBar extends React.Component {
           </div>
           <div className="flex space-x-4">
             <a
-              href="https://www.linkedin.com/in/yourprofile"
+              href="https://www.linkedin.com/in/ryan-flynn-5b0a56170/"
               target="_blank"
               rel="noopener noreferrer"
               className="text-white"
+              onClick={(e) =>
+                ReactGA.event({ category: "Link Clicked", action: "LinkedIn" })
+              }
             >
               <FaLinkedin />
             </a>
             <a
-              href="https://github.com/yourprofile"
+              href="https://github.com/rflynn523"
               target="_blank"
               rel="noopener noreferrer"
               className="text-white"
+              onClick={(e) =>
+                ReactGA.event({
+                  category: "Link Clicked",
+                  action: "Github Profile",
+                })
+              }
             >
               <FaGithub />
             </a>
             <a
-              href="link-to-your-resume.pdf"
+              href={resumePDF}
               target="_blank"
               rel="noopener noreferrer"
               className="text-white"
             >
               <FaFilePdf />
             </a>
+            {/* <a href={savingsMapperPrivacyPolicy} target="_blank">
+            Savings Mapper Privacy Policy
+          </a> */}
           </div>
         </div>
       </nav>
