a6eab0805819d9dcc33a572484168fbd6b8c3154
rflynn523 <rflynn523@yahoo.com>
2024-07-20 23:00:11 -0400

    added more padding to top

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -55,71 +55,73 @@ class NavBar extends React.Component {
       // <nav className="fixed w-full h-fit top-0 z-50 bg-nav-bar-color3 bg-opacity-70 backdrop-blur-md py-5 text-app-text-color">
 
       <nav className="fixed w-full h-fit top-0 z-50 bg-gradient-to-r from-middarkside2 via-teal5 to-middarkside2 text-nav-bar-font-color">
-        <div className="container mx-auto flex justify-between items-center py-5">
-          {/* Jump To Buttons */}
-          <div className="flex flex-1 space-x-8 text-nav-bar-font-color text-lg">
-            <button onClick={() => scrollToSection("Home")}>Home</button>
-            <button onClick={() => scrollToSection("About Me")}>
-              About Me
-            </button>
-            <button onClick={() => scrollToSection("Projects")}>
-              Projects
-            </button>
-            <button onClick={() => scrollToSection("Work")}>Work</button>
-          </div>
-
-          {/* Name */}
-          <div className="text-nav-bar-font-color poppins-semibold tracking-wide absolute left-1/2 transform -translate-x-1/2">
-            <div
-              className={`transition-all ease-in-out duration-700 ${this.state.isScrolled ? "text-4xl" : "text-6xl"}`}
-            >
-              Ryan Flynn
+        <div className={`transition-all ease-in-out duration-700 ${this.state.isScrolled ? "pt-0" : "pt-6"}`}>
+          <div className="container mx-auto flex justify-between items-center py-5">
+            {/* Jump To Buttons */}
+            <div className="flex flex-1 space-x-8 text-nav-bar-font-color text-lg">
+              <button onClick={() => scrollToSection("Home")}>Home</button>
+              <button onClick={() => scrollToSection("About Me")}>
+                About Me
+              </button>
+              <button onClick={() => scrollToSection("Projects")}>
+                Projects
+              </button>
+              <button onClick={() => scrollToSection("Work")}>Work</button>
             </div>
-          </div>
 
-          {/* Contact Links */}
-          <div className="flex space-x-6">
-            {/* LinkedIn */}
-            <a
-              href="https://www.linkedin.com/in/ryan-flynn-5b0a56170/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white"
-              onClick={(e) =>
-                ReactGA.event({ category: "Link Clicked", action: "LinkedIn" })
-              }
-            >
-              <FaLinkedin className="size-8" />
-            </a>
+            {/* Name */}
+            <div className="text-nav-bar-font-color poppins-semibold tracking-wide absolute left-1/2 transform -translate-x-1/2">
+              <div
+                className={`transition-all ease-in-out duration-700 ${this.state.isScrolled ? "text-4xl" : "text-6xl"}`}
+              >
+                Ryan Flynn
+              </div>
+            </div>
 
-            {/* GitHub */}
-            <a
-              href="https://github.com/rflynn523"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white"
-              onClick={(e) =>
-                ReactGA.event({
-                  category: "Link Clicked",
-                  action: "Github Profile",
-                })
-              }
-            >
-              <FaGithub className="size-8" />
-            </a>
+            {/* Contact Links */}
+            <div className="flex space-x-6">
+              {/* LinkedIn */}
+              <a
+                href="https://www.linkedin.com/in/ryan-flynn-5b0a56170/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+                onClick={(e) =>
+                  ReactGA.event({ category: "Link Clicked", action: "LinkedIn" })
+                }
+              >
+                <FaLinkedin className="size-8" />
+              </a>
 
-            {/* Resume */}
-            <a
-              href={resumePDF}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-white"
-            >
-              <FaFilePdf className="size-8" />
-            </a>
-            {/* <a href={savingsMapperPrivacyPolicy} target="_blank">
-            Savings Mapper Privacy Policy
-          </a> */}
+              {/* GitHub */}
+              <a
+                href="https://github.com/rflynn523"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+                onClick={(e) =>
+                  ReactGA.event({
+                    category: "Link Clicked",
+                    action: "Github Profile",
+                  })
+                }
+              >
+                <FaGithub className="size-8" />
+              </a>
+
+              {/* Resume */}
+              <a
+                href={resumePDF}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <FaFilePdf className="size-8" />
+              </a>
+              {/* <a href={savingsMapperPrivacyPolicy} target="_blank">
+              Savings Mapper Privacy Policy
+            </a> */}
+            </div>
           </div>
         </div>
       </nav>
