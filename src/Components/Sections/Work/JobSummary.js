5f9df13b73ac864d6f46f7d06bed29c2dbc32e25
rflynn523 <rflynn523@yahoo.com>
2024-08-03 17:03:47 -0400

    Job Summary tailwinds

--- src/Components/Sections/Work/JobSummary.js
+++ src/Components/Sections/Work/JobSummary.js
@@ -1,47 +1,24 @@
 import React from "react";
-import ReactGA from "react-ga";
-
-import { Divider, Grid, Typography } from "@mui/material";
-
-const card = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  height: "100%",
-
-  // Needed for the spacing of each "Job Accordion"
-  marginBottom: "20px",
-  marginRight: "50px",
-  marginLeft: "50px",
-};
 
 class JobSummary extends React.Component {
   render() {
     return (
-      <Grid container spacing={2} alignItems="center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
         {/* Job Title */}
-        <Grid item xs={12} sm={6} md={4}>
-          <Typography variant="h6">{this.props.job_info.title}</Typography>
-        </Grid>
+        <div className="text-left md:text-left">
+          <div className="truncate">{this.props.job_info.title}</div>
+        </div>
 
         {/* Company Name */}
-        <Grid item xs={12} sm={6} md={3}>
-          <Typography variant="subtitle1">
-            {this.props.job_info.company}
-          </Typography>
-        </Grid>
-
-        <Grid item xs={12} sm={6} md={1}>
-          <Divider orientation="vertical" />
-        </Grid>
+        <div className="text-center">
+          <div className="truncate">{this.props.job_info.company}</div>
+        </div>
 
         {/* Dates */}
-        <Grid item xs={12} sm={6} md={3}>
-          <Typography variant="subtitle1">
-            {this.props.job_info.date}
-          </Typography>
-        </Grid>
-      </Grid>
+        <div className="text-right md:text-right">
+          <div>{this.props.job_info.date}</div>
+        </div>
+      </div>
     );
   }
 }
--- src/Components/Sections/Work/Work.js
+++ src/Components/Sections/Work/Work.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactGA from "react-ga";
 
-import "./Work.css";
 import JobSummary from "./JobSummary";
 import JobDetails from "./JobDetails";
 import work_experience_data from "./work-data";
@@ -27,38 +26,15 @@ class Work extends React.Component {
                 <div className="bg-slate bg-opacity-10 backdrop-blur-lg rounded-lg overflow-hidden">
                   {/* Header of the card with job summary and toggle button */}
                   <div
-                    className="px-4 py-2 flex items-center rounded-lg justify-between cursor-pointer border border-transparent hover:border-with-green transition-all duration-300"
+                    className="px-4 py-2 rounded-lg cursor-pointer border border-transparent hover:border-with-green transition-all duration-300"
                     onClick={() => {
                       const content = document.getElementById(
-                        `panel-${index}-content`,
+                        `panel-${index}-content`
                       );
                       content.classList.toggle("hidden");
                     }}
                   >
                     <JobSummary job_info={job} />
-                    {/* Button to toggle the accordion content */}
-                    <button
-                      className="focus:outline-none"
-                      aria-controls={`panel-${index}-content`}
-                      aria-expanded="false"
-                      id={`panel-${index}-header`}
-                    >
-                      {/* SVG icon for expand/collapse */}
-                      <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M19 9l-7 7-7-7"
-                        />
-                      </svg>
-                    </button>
                   </div>
                   {/* Content of the accordion, initially hidden */}
                   <div
