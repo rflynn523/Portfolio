1e9a6a945bdb083d33d569a3a1f93aed203387bd
rflynn523 <rflynn523@yahoo.com>
2024-08-04 17:55:23 -0400

    styled the links location

--- src/Components/Sections/Projects/ProjectCard.js
+++ src/Components/Sections/Projects/ProjectCard.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { FaAppStore, FaGithub, FaYoutube } from "react-icons/fa"; // Import icons from react-icons
 
 import TechChip from "../../TechChip";
 
 class ProjectCard extends React.Component {
   render() {
     const { project } = this.props;
+
+    // Helper function to determine the appropriate icon for the link
+    const getLinkIcon = (url) => {
+      if (url.includes("github.com")) return <FaGithub size={32} />;
+      if (url.includes("youtube.com")) return <FaYoutube size={32} />;
+      if (url.includes("apps.apple.com")) return <FaAppStore size={32} />;
+
+      return null;
+    };
 
     return (
       <div className="flex items-center h-full bg-slate bg-opacity-10 backdrop-blur-lg p-5 mx-auto rounded-lg shadow-lg">
@@ -17,14 +27,28 @@ class ProjectCard extends React.Component {
 
         {/* Everything else on the right */}
         <div className="w-2/3 flex flex-col justify-between space-y-8">
-          {/* Title */}
-          <h2 className="text-2xl font-bold mb-4">{project.title}</h2>
+          <div className="flex justify-between pr-8 space-x-4">
+            {/* Title */}
+            <h2 className="text-2xl font-bold">{project.title}</h2>
+
+            {/* Links */}
+            <div className="flex space-x-4">
+              {Object.entries(project.links).map(([url, label]) => (
+                <a
+                  key={url}
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-gray-700 hover:text-gray-900"
+                >
+                  {getLinkIcon(url)}
+                </a>
+              ))}
+            </div>
+          </div>
 
           {/* Description */}
           <div className="mb-4">{project.description}</div>
-
-          {/* Links */}
-          <div className="mb-4">Links</div>
 
           {/* Tech Used */}
           <div className="mt-auto">
