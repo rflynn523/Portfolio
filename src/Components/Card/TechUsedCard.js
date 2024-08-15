678fd22f8f9aa409c9e6ae5a795593def16a2b3a
rflynn523 <rflynn523@yahoo.com>
2024-08-14 22:19:32 -0400

    tech responsive design

--- src/Components/Card/TechUsedCard.js
+++ src/Components/Card/TechUsedCard.js
@@ -8,18 +8,18 @@ class TechUsedCard extends React.Component {
 
   render() {
     return (
-      <div className="bg-slate bg-opacity-10 backdrop-blur-lg p-5 rounded-lg shadow-lg">
-        <div className="flex text-xl font-bold mb-5 border-2 rounded-full justify-center">
+      <div className="bg-slate bg-opacity-10 backdrop-blur-lg p-4 md:p-5 rounded-lg shadow-lg">
+        <div className="text-xl font-bold mb-4 border-2 rounded-full flex items-center justify-center p-2">
           {this.props.title}
         </div>
-        <div className="flex flex-wrap gap-4 justify-evenly">
+        <div className="flex flex-wrap gap-2 sm:gap-4 justify-evenly">
           {this.props.logos.map((logo, logoIndex) => {
             return (
               <Tooltip key={logoIndex} text={logo.tooltip}>
                 <img
                   src={logo.image}
                   alt={`Image ${logoIndex + 1}`}
-                  className="size-16 object-contain"
+                  className="size-12 sm:size-16 object-contain"
                 />
               </Tooltip>
             );
