9055f8314d48c68a299a1fea8fb40342d5cad00c
rflynn523 <rflynn523@yahoo.com>
2025-05-13 22:40:27 -0400

    fixed App imports

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 import NavBar from "../NavBar/NavBar";
-import AboutMe from "../sections_temp/AboutMe/AboutMe";
-import Technologies from "../sections_temp/Technologies/Technologies";
-import Projects from "../sections_temp/Projects/Projects";
-import Experiences from "../sections_temp/Experiences/Experiences";
+import AboutMe from "../sections/AboutMe/AboutMe";
+import Technologies from "../sections/Technologies/Technologies";
+import Projects from "../sections/Projects/Projects";
+import Experiences from "../sections/Experiences/Experiences";
 import "./App.css";
 
 class App extends React.Component {
