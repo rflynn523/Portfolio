ce9e532225a77d4a5764eac928f4ed6c27d89a08
rflynn523 <rflynn523@yahoo.com>
2026-09-09 00:07:49 -0400

    updated technologies

--- src/Components/sections/Technologies/Technologies.js
+++ src/Components/sections/Technologies/Technologies.js
@@ -4,23 +4,14 @@ import TechUsedCard from "./TechUsedCard";
 import tech_used_data from "./tech-used-data";
 
 class Technologies extends React.Component {
-  card_info = [
-    tech_used_data.prog_lang,
-    tech_used_data.libraries,
-    tech_used_data.tools,
-    tech_used_data.databases,
-    tech_used_data.devops,
-    tech_used_data.hardware,
-  ];
-
   render() {
     return (
       <div className="pt-20 xl:pt-24 px-4 lg:px-16">
         {/* Grid for the Tech Used */}
         <Title text="Technologies" />
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 lg:gap-10 sm:mx-4 lg:mx-8">
-          {this.card_info.map((card, index) => (
-            <div key={card.id}>
+          {Object.values(tech_used_data).map((card) => (
+            <div key={card.title}>
               <TechUsedCard title={card.title} logos={card.logos} />
             </div>
           ))}
